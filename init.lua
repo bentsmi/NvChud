17184a73c02de5c5eb027bdeaad24c97236a0f14
Ben Smith <smithbenjamin2005@gmail.com>
2026-03-14 17:46:13 -0400

    git init

--- init.lua
+++ init.lua
@@ -6,4 +6,29 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function() vim.hl.on_yank() end,
+})
+
+vim.keymap.set("n", " x", ":source<CR>")
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
+
+require("rose-pine").setup({
+    styles = {
+        italic = false,
+    },
+})
+
 vim.cmd("colorscheme rose-pine")
+
+-- Init plugins here
+require("oil").setup({
+    view_options = {
+        show_hidden = true,
+    },
+})
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+require("nvim-web-devicons").setup()
